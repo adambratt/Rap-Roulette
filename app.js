61c66f4aa9eb11e9cfadfe012b5eb6ac7d32b1c3
root <root@rapper.(none)>
2012-01-21 07:05:00 +0000

    fixed

--- app.js
+++ app.js
@@ -3,5 +3,5 @@ var http = require('http');
 http.createServer(function (req, res) {
     res.writeHead(200, {'Content-Type': 'text/plain'});
     res.end('Hello World\n');
-}).listen(80, "127.0.0.1");
+}).listen(80, "127.0.0.1);
 console.log('Server running at http://127.0.0.1:80/');
